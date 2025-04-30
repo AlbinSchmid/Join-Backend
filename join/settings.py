b08adaf6f2208b8a5c27781b728bd2ff4dafea1e
Albin Schmid <schmidalbin15@gmail.com>
2025-04-30 15:08:52 +0200

    deploy test

--- join/settings.py
+++ join/settings.py
@@ -70,7 +70,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:4200',
     'http://localhost:4200',
     'http://join.albin-schmid.com',
-    'https://join.albin-schmid.com',
+    'https://join.albin-schmid.com'
 ]
 
 
